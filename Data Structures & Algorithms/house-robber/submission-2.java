5b3fb5a14818eb8929052a471a3601214bb0a864
Udbhas Dutta <udbhasdutta@gmail.com>
2026-09-14 09:20:52 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return helper(nums, nums.length-1, dp);
+
+    }
+    public int helper(int[] nums, int index, int[] dp){
+        if(index < 0) return 0;
+
+        if(dp[index] != -1) return dp[index];
+
+        int pick = nums[index] + helper(nums, index-2, dp);
+        int notPick = helper(nums, index-1, dp);
+
+        return dp[index] = Math.max(pick, notPick);
+    }
+}
